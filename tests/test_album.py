ffceb9f77c70a92340877508747b0b83078cb606
isinning1 <i.sinning@uniandes.edu.co>
2023-09-11 14:45:57 -0500

    Pruebas que funcionan , se crea metodo (GREEN)

--- tests/test_album.py
+++ tests/test_album.py
@@ -4,5 +4,8 @@ from src.logica.coleccion import Coleccion
 from src.modelo.album import Album, Medio
 from src.modelo.declarative_base import Session
 
-class AlbumTestCase(unittest.TestCase)
+class AlbumTestCase(unittest.TestCase):
+
+    def test_prueba(self):
+        self.assertEqual(0, 0)
     
