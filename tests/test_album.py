63c76cd976a3af8c458d37d388b3b03bd4e3f866
isinning1 <i.sinning@uniandes.edu.co>
2023-09-11 14:42:39 -0500

    Actualizacion de codigo (Red)

---
+++ tests/test_album.py
@@ -0,0 +1,8 @@
+import unittest
+
+from src.logica.coleccion import Coleccion
+from src.modelo.album import Album, Medio
+from src.modelo.declarative_base import Session
+
+class AlbumTestCase(unittest.TestCase)
+    
